4462b13f455a69a27c54b07c983475f79099f879
kintokeanu <brianislevu@gmail.com>
2023-03-15 20:43:46 +0300

    count args passed

--- argsargv/argsCount.c
+++ argsargv/argsCount.c
@@ -4,8 +4,8 @@ int main(int argc, char *argv[])
 {
     int i;
     
-    printf("Program name: \n", argv[0]);
-    printf("Number of arguments passed: \n", argc - 1);
+    printf("Program name: %s\n", argv[0]);
+    printf("Number of arguments passed: %d\n", argc - 1);
 
     for ( i = 1; i < argc; i++)
     {
