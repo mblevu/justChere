a1bce8168b41c5092f0e32f5b3def6c98088c966
kintokeanu <brianislevu@gmail.com>
2023-03-18 17:49:15 +0300

    display 8x8 chessboard

---
+++ morepointers/chessboard.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * take pointer to 8x8 array as argument
+ * print out the contents
+*/
+
+void print_chessboard(char(*a)[8])
+{
+    int i, j;
+
+    for (i = 0; i < 8; i++)
+    {
+        for (j = 0; j < 8; j++)
+        {
+            printf("%c ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(void)
+{
+    char board[8][8] = {    {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+    {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+    {'.', '.', '.', '.', '.', '.', '.', '.'},
+    {'.', '.', '.', '.', '.', '.', '.', '.'},
+    {'.', '.', '.', '.', '.', '.', '.', '.'},
+    {'.', '.', '.', '.', '.', '.', '.', '.'},
+    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+    {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}
+};
+print_chessboard(board);
+}
