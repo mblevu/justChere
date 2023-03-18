cc15730762c9676f013aec4733faba3ce72fc5ba
kintokeanu <brianislevu@gmail.com>
2023-03-18 13:43:11 +0300

    update header file

---
+++ pointers/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void print_reverse(char *str);
+char *_strcpy(char *dest, const char *src);
+char *_strcat(char *dest, char *src);
+char * _strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+void print_array(int *a, int n);
+char *leet(char *);
+char *rot13(char *str);
+
+
+#endif
