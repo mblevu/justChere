c71fbbad742553a9dccc58af87df22e64f2fa68e
kintokeanu <brianislevu@gmail.com>
2023-03-18 16:54:41 +0300

    search string for any set of bytes

---
+++ morepointers/strpbrk.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ *  
+ * check string for same elements in both
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+    char *p1, *p2;
+
+    for (p1 = s; * p1 != '\0'; p1++)
+    {
+        for (p2 = accept; * p2 != '\0'; p2++)
+        {
+            if (* p1 == * p2)
+            {
+                return (p1);
+            }
+        }
+    }
+    return (NULL);
+      
+}
+
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
+
+    t = _strpbrk(s, f);
+    printf("%s\n", t);
+    return (0);
+}
