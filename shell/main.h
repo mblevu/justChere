a63b7943a97169ec9813beb0d0ff24a15d3bff42
kintokeanu <brianislevu@gmail.com>
2023-04-13 00:59:17 +0300

    header file

---
+++ shell/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+
+
+
+
+
+
+
+
+#endif
